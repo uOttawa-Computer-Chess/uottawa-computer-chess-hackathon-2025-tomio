8b03407a7e6d00910f36a73b737a9a5dd195d5de
tomio <temujin142857@gmail.com>
2025-10-25 18:11:40 -0400

    added more time for less pieces

--- homemade.py
+++ homemade.py
@@ -72,8 +72,10 @@ class MyBot(ExampleEngine):
         inc = my_inc if isinstance(my_inc, (int, float)) else 0
         budget = (remaining or 0) + 2 * inc  # crude increment bonus
         total_depth = 4
-        if(eval.num_pieces(board)<7):
+        if(eval.num_pieces(board)<10):
             total_depth=5 
+        if(eval.num_pieces(board)<7):
+            total_depth=6 
         if(remaining<10):
             total_depth=3    
         startTime=time.time()
